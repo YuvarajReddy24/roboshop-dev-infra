1723c423b9ced2089c191bda9cb1d9c250d39495
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-25 20:40:08 +0530

    main.tf

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,5 +1,6 @@
 # Using Open source module
-module "catalogue" {
+
+/* module "catalogue" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "${local.common_name_suffix}-catalogue"
@@ -7,7 +8,7 @@ module "catalogue" {
   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
-} 
+} */
 
 module "sg" {
   count = length(var.sg_names)
