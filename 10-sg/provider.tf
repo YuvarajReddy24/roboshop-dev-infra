e9ee928730bf6c994af57be87dceadf053b1f595
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-29 19:15:29 +0530

    .

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "yuvarajreddy-1-dev"
-    key    = "roboshop-dev-vpc"
+    key    = "roboshop-dev-sg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
